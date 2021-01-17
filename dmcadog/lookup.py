d1b23cb18ff7673d174a1b554966b19b012cde08
rodeyfeld <eric.d.rodefeld@gmail.com>
2021-01-17 16:23:47 -0500

    Connected AJAX request to lookup API

--- dmcadog/lookup.py
+++ dmcadog/lookup.py
@@ -1,12 +1,14 @@
-import functools
+from .lumen_api import LumenAPI
 from flask import Blueprint, request, render_template
-
 bp = Blueprint('lookup', __name__, url_prefix='/')
 
 
-@bp.route('/lookup/', methods=('POST', 'GET'))
+@bp.route('/lookup', methods=('POST', 'GET'))
 def lookup():
+
     if request.method == 'POST':
-        print(request.json['query'])
-        pass
-    return render_template('lookup.html')
+        data = request.get_json()
+        api = LumenAPI()
+        total = api.process(data)
+        return total
+    return None
--- dmcadog/lumen_api.py
+++ dmcadog/lumen_api.py
@@ -3,9 +3,20 @@ from bs4 import BeautifulSoup
 
 
 class LumenAPI:
-    base_url: str = "https://lumendatabase.org/notices/search"
+    """
+     A class for querying the Lumen DMCA database. Uses Beautiful Soup to
+     parse results.
+    """
+
+    def __init__(self):
+        self.base_url: str = "https://lumendatabase.org/notices/search"
 
     def fetch(self, query: str) -> str:
+        """
+        Connects to Lumen page and fetches HTML
+        :param query: string that will be lookup up in Lumen
+        :return: string with all HTML text
+        """
         params = {
             'utf8': '✓',
             'country_code_facet': 'US',
@@ -19,6 +30,11 @@ class LumenAPI:
 
     @staticmethod
     def get_content(response_text):
+        """
+        Beautiful soup parser for HTML
+        :param response_text: HTML string containing Lumen data
+        :return: String containing total number of results
+        """
         soup = BeautifulSoup(response_text, 'html.parser')
         try:
             return soup.find('span', class_='total-entries').get_text()
@@ -26,6 +42,11 @@ class LumenAPI:
             return "Failed to get results"
 
     def process(self, query):
+        """
+        Primary access route for API requests
+        :param query: string that will be lookup up in Lumen
+        :return: string containing results
+        """
         response_text = self.fetch(query)
         content = self.get_content(response_text)
         return content
